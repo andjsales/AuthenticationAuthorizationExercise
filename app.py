5317b56b388b11336049c839b9c78294f15372ef
andrew.sales <andjsales@gmail.com>
2023-11-09 16:56:48 -0600

    final changes

--- app.py
+++ app.py
@@ -26,13 +26,16 @@ def inject_user():
 
 @app.route('/')
 def homepage():
+    """
+    redirects to register page 
+    """
     return redirect('/register')
 
 
 @app.route('/register')
 def show_register_form():
     """
-    - show registration form 
+    generates registration form 
     """
     form = RegistrationForm()
     return render_template('register.html', form=form)
@@ -40,6 +43,9 @@ def show_register_form():
 
 @app.route('/register', methods=["POST"])
 def process_register_form():
+    """
+    Shows a form that when submitted will register/create a user
+    """
     form = RegistrationForm(request.form)
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(
@@ -88,23 +94,31 @@ def process_login_form():
 
 @app.route('/users/<string:username>')
 def show_user(username):
+    """
+    Shows information about given user
+    Shows all feedback from user
+    """
     if 'user_id' not in session:
         flash('You must be logged in to view this page.', 'danger')
         return redirect('/login')
 
     logged_in_user = User.query.get(session['user_id'])
+    user = User.query.filter_by(username=username).first_or_404()
 
     if logged_in_user.username != username:
         flash('You are not authorized to view this page.', 'danger')
         return redirect('/login')
 
     feedback = Feedback.query.filter_by(user_id=logged_in_user.id).all()
-
-    return render_template('secret.html', user=logged_in_user, feedback=feedback, username=logged_in_user.username)
+    return render_template('secret.html', user=user, feedback=feedback)
 
 
 @app.route('/users/<string:username>/delete', methods=['POST'])
 def delete_user(username):
+    """
+    Remove the user from the database 
+    Deletes all of their feedback
+    """
     if 'username' not in session or username != session['username']:
         flash('You are not authorized to perform this action.', 'danger')
         return redirect('/login')
@@ -120,6 +134,10 @@ def delete_user(username):
 
 @app.route('/users/<string:username>/feedback/add', methods=['GET', 'POST'])
 def add_feedback(username):
+    """
+    Display a form to add feedback
+    Add a new piece of feedback
+    """
     if 'user_id' not in session:
         flash('You must be logged in to add feedback.', 'danger')
         return redirect(url_for('show_login_form'))
@@ -139,17 +157,22 @@ def add_feedback(username):
         db.session.add(new_feedback)
         db.session.commit()
         flash('Feedback added!', 'success')
-        return redirect(url_for('add_feedback', username=logged_in_user.username))
+        return redirect(url_for('show_user', username=logged_in_user.username))
     return render_template('add_feedback.html', form=form, username=username)
 
 
 @app.route('/feedback/<int:feedback_id>/update', methods=['GET', 'POST'])
 def update_feedback(feedback_id):
+    """
+    Display a form to edit feedback
+    Update a specific piece of feedback
+    """
     feedback = Feedback.query.get_or_404(feedback_id)
+    logged_in_user = User.query.get(session['user_id'])
 
-    if 'username' not in session or feedback.username != session['username']:
+    if feedback.user_id != logged_in_user.id:
         flash('You do not have permission to edit this feedback.', 'danger')
-        return redirect(url_for('homepage'))
+        return redirect(url_for('show_user', username=logged_in_user.username))
 
     form = FeedbackForm(obj=feedback)
 
@@ -158,27 +181,34 @@ def update_feedback(feedback_id):
         feedback.content = form.content.data
         db.session.commit()
         flash('Feedback updated!', 'success')
-        return redirect(url_for('show_user', username=session['username']))
+        return redirect(url_for('show_user', username=logged_in_user.username))
 
-    return render_template('update_feedback.html', form=form)
+    return render_template('update_feedback.html', form=form, feedback_id=feedback_id)
 
 
-@app.route('/feedback/<int:feedback_id>/delete', methods=['POST'])
+@app.route('/feedback/<int:feedback_id>/delete', methods=['GET', 'POST'])
 def delete_feedback(feedback_id):
+    """
+    Delete a specific piece of feedback
+    """
     feedback = Feedback.query.get_or_404(feedback_id)
+    logged_in_user = User.query.get(session['user_id'])
 
-    if 'username' not in session or feedback.username != session['username']:
+    if feedback.user_id != logged_in_user.id:
         flash('You do not have permission to delete this feedback.', 'danger')
-        return redirect(url_for('homepage'))
+        return redirect(url_for('show_user', username=logged_in_user.username))
 
     db.session.delete(feedback)
     db.session.commit()
     flash('Feedback deleted!', 'success')
-    return redirect(url_for('show_user', username=session['username']))
+    return redirect(url_for('show_user', username=logged_in_user.username))
 
 
 @app.route('/logout')
 def logout_user():
+    """
+    Logout the current user
+    """
     session.pop('user_id')
     flash("Goodbye!", "info")
     return redirect('/login')
